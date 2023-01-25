d9e00a5b4a0b97603da429cbf5420bd1369c2e81
Mauro Camara Escudero <maurocamara@Mauros-MacBook-Pro-2.local>
2023-01-25 15:46:27 +0000

    add constrained random walk metropolis

---
+++ ConstrainedRWM.py
@@ -0,0 +1,146 @@
+from numpy import inf, zeros, log, finfo
+from numpy.random import rand, randn
+from numpy.linalg import solve, norm, cond
+import scipy.linalg as la
+
+
+def zappa_sampling_storecomps_rattle_manifold(x0, manifold, n, T, B, tol, rev_tol, maxiter=50, norm_ord=2):
+    """C-RWM Rattle. This was used in G and K experiment."""
+    assert type(B) == int
+    assert norm_ord in [2, inf]
+    assert len(x0) == manifold.n, "Initial point has wrong dimension."
+    # Check arguments
+    n = int(n)  
+    B = int(B)
+    δ = T / B
+    d, m = manifold.get_dimension(), manifold.get_codimension()
+
+    # Initial point on the manifold
+    x = x0
+    compute_J = lambda x: manifold.J(x) #manifold.Q(x).T
+
+    # House-keeping
+    samples = zeros((n, d + m))    # Store n samples on the manifold
+    samples[0, :] = x
+    i = 1
+    N_EVALS = {'jacobian': 0, 'density': 0}
+    ACCEPTED = zeros(n)
+    # Define function to compute density
+    def logη(x):
+        """Computes log density on Manifold but makes sure everything is behaving nicely."""
+        return manifold.logη(x) #manifold.logpost(x)
+
+    # Log-uniforms for MH accept-reject step
+    logu = log(rand(n))
+
+    # Compute jacobian & density value
+    Jx    = compute_J(x) #manifold.Q(x).T
+    logηx = logη(x)
+    N_EVALS['jacobian'] += 1
+    N_EVALS['density'] += 1
+    
+    def linear_project(v, J):
+        """Projects by solving linear system."""
+        return J.T @ solve(J@J.T, J@v)
+        #return J.T.dot(solve(J.dot(J.T), J.dot(v)))
+
+    # Constrained Step Function
+    def constrained_rwm_step(x, v, tol, maxiter, Jx, norm_ord=norm_ord):
+        """Used for both forward and backward. See Manifold-Lifting paper."""
+        # Project momentum
+        v_projected = v - linear_project(v, Jx) 
+        # Unconstrained position step
+        x_unconstr = x + v_projected
+        # Position Projection
+        a, flag, n_grad = project_zappa_manifold(manifold, x_unconstr, Jx.T, tol, maxiter, norm_ord=norm_ord)
+        y = x_unconstr - Jx.T @ a 
+        try:
+            Jy = compute_J(y) 
+        except ValueError as e:
+            print("Jacobian computation at projected point failed. ", e)
+            return x, v, Jx, 0, n_grad + 1
+        # backward velocity
+        v_would_have = y - x
+        # Find backward momentum & project it to tangent space at new position
+        v_projected_endposition = v_would_have - linear_project(v_would_have, Jy) #qr_project(v_would_have, Jy) #qr_project((y - x) / δ, Jy)
+        # Return projected position, projected momentum and flag
+        return y, v_projected_endposition, Jy, flag, n_grad + 1
+    
+    def constrained_leapfrog(x0, v0, J0, B, tol, rev_tol, maxiter, norm_ord=norm_ord):
+        """Constrained Leapfrog/RATTLE."""
+        successful = True
+        n_jacobian_evaluations = 0
+        x, v, J = x0, v0, J0
+        for _ in range(B):
+            xf, vf, Jf, converged_fw, n_fw = constrained_rwm_step(x, v, tol, maxiter, J, norm_ord=norm_ord)
+            xr, vr , Jr, converged_bw, n_bw = constrained_rwm_step(xf, -vf, tol, maxiter, Jf, norm_ord=norm_ord)
+            n_jacobian_evaluations += (n_fw + n_bw)  # +2 due to the line Jy = manifold.Q(y).T
+            if (not converged_fw) or (not converged_bw) or (norm(xr - x, ord=norm_ord) >= rev_tol):
+                successful = False
+                return x0, v0, J0, successful, n_jacobian_evaluations
+            else:
+                x = xf
+                v = vf
+                J = Jf
+        return x, v, J, successful, n_jacobian_evaluations
+
+    for i in range(n):
+        v = δ*randn(m + d) # Sample in the ambient space.
+        xp, vp, Jp, LEAPFROG_SUCCESSFUL, n_jac_evals = constrained_leapfrog(x, v, Jx, B, tol=tol, rev_tol=rev_tol, maxiter=maxiter)
+        N_EVALS['jacobian'] += n_jac_evals
+        if LEAPFROG_SUCCESSFUL:
+            logηp = logη(xp)
+            N_EVALS['density'] += 1
+            if logu[i] <= logηp - logηx - (vp@vp)/2 + (v@v)/2: 
+                # Accept
+                ACCEPTED[i - 1] = 1
+                x, logηx, Jx = xp, logηp, Jp
+                samples[i, :] = xp
+            else:
+                # Reject
+                samples[i, :] = x
+                ACCEPTED[i - 1] = 0
+        else:
+            # Reject
+            samples[i, :] = x
+            ACCEPTED[i - 1] = 0
+    return samples, N_EVALS, ACCEPTED
+
+
+def project_zappa_manifold(manifold, z, Q, tol = 1.48e-08 , maxiter = 50, norm_ord=2):
+    '''
+    USED IN G AND K. This version is the version of Miranda & Zappa. It retuns i, the number of iterations
+    i.e. the number of gradient evaluations used.
+    '''
+    a, flag, i = zeros(Q.shape[1]), 1, 0
+
+    # Compute the constrained at z - Q@a. If it fails due to overflow error, return a rejection altogether.
+    try:
+        projected_value = manifold.q(z - Q@a)
+    except ValueError as e:
+        return a, 0, i
+    # While loop
+    while la.norm(projected_value, ord=norm_ord) >= tol:
+        try:
+            Jproj = manifold.Q(z - Q@a).T
+        except ValueError as e:
+            print("Jproj failed. ", e)
+            return zeros(Q.shape[1]), 0, i
+        # Check that Jproj@Q is invertible. Do this by checking condition number 
+        # see https://stackoverflow.com/questions/13249108/efficient-pythonic-check-for-singular-matrix
+        GramMatrix = Jproj@Q
+        if cond(GramMatrix) < 1/finfo(z.dtype).eps:
+            Δa = la.solve(GramMatrix, projected_value)
+            a += Δa
+            i += 1
+            if i > maxiter:
+                return zeros(Q.shape[1]), 0, i
+            # If we are not at maxiter iteration, compute new projected value
+            try:
+                projected_value = manifold.q(z - Q@a)
+            except ValueError as e:
+                return zeros(Q.shape[1]), 0, i
+        else:
+            # Fail
+            return zeros(Q.shape[1]), 0, i
+    return a, 1, i
